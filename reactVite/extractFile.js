e1a4d5f52fae11689fe9431289d0cf83beb6f614
anjani.kumar <anjani.kumar@sprinklr.com>
2024-06-14 16:23:47 +0530

    Made the final case and only one left whic cant be covered

--- reactVite/extractFile.js
+++ reactVite/extractFile.js
@@ -1,16 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-control-regex */
 
-import fs from 'fs';
-import addMocksvariable from './test3.js';
+import fs, { chownSync } from 'fs';
+import addMocksvariable from './test2.js';
+import { open } from 'node:fs/promises';
 
 const regexASNI= /\u001b\[\d+m/g;
 
-const map = new Map();
+
+
 const elements = [];
 
 
-fs.readFile('output.txt', 'utf-8', (err, data) => {
+async function scanTestAndLineNumberFromFile(fileName) {
+    const file = await open(fileName);
+    const testAndLineArray = new Array();
+    const testRegex = /^(?!.*\/\/)(?!.*\/\*).*test\(("|')([^\1)]+)\1/gm;
+    let lineNumber = 1;
+    for await (const line of file.readLines()) {
+        // read the 1st argumnet of test, that is test name, if its not commented
+        const isTestFound = testRegex.exec(line);
+        if (isTestFound!=null) {
+            testAndLineArray.push({ line: lineNumber, test: isTestFound[2] });
+          //  console.log(isTestFound);
+        }
+        lineNumber++;
+    }
+    return testAndLineArray;
+}
+
+fs.readFile('output.txt', 'utf-8',async (err, data) => {
     if (err) console.log(err);
     let str = data.toString();
     str = str.replace(regexASNI, '');
@@ -19,25 +38,42 @@ fs.readFile('output.txt', 'utf-8', (err, data) => {
     const regex = /No more mocked responses((.|\n)*?)Expected variables:((.|\n)*?)at Object.<anonymous> \(((.|\n)*?)\n/gm;
    
     let match = "";
+    const map = new Map();
+    let lastLineinserted = -1;
     while ((match = regex.exec(str)) !== null) {
     
         const group1 = match[1];
         const group2 = match[3];
         const group3 = match[5];
         let query = group1.split(' for the query:')[1].trim();
-        let queryVariable = query.split('(')[0].split(' ')[1];
+        let queryName = query.split('(')[0].split(' ')[1];
 
-        const constVariableArray = queryVariable.match(/[A-z][a-z]+/g);
+        const constVariableArray = queryName.match(/[A-z][a-z]+/g);
         let constVariable = constVariableArray.join("_");
         constVariable =constVariable.concat("_QUERY").toUpperCase();
-        let variables = group2.split('This typically indicates a configuration error in your mocks setup, usually due to a typo or mismatched variable')[0].trim();
+        const splitArray = group2.split('This typically indicates a configuration error in your mocks setup, usually due to a typo or mismatched variable');
+        const variables = splitArray[0].trim();
+        const regexRender = />(.*)render\(/gm;
+        const lineNumberAndNise = regexRender.exec(splitArray[1])[1].trim();
+        const lineNumber = lineNumberAndNise.split(' ')[0];
+        //console.log(lineNumber);
+        const queryVariable = constVariable + '$#' + variables;
         let fileName = group3.split(':')[0].trim();
+        if (lastLineinserted === lineNumber) continue;
         if (map.has(fileName)) {
-            map.get(fileName).push([query,constVariable, variables]);
+            const mapQueryVaribles = map.get(fileName);
+            if (mapQueryVaribles.has(queryVariable)) {
+                (map.get(fileName)).get(queryVariable).push(lineNumber);
+            } else {
+                map.get(fileName).set(queryVariable, [lineNumber]);
+            }
         } else {
-            map.set(fileName, [[query,constVariable, variables]]);
+            const mapQueryVaribles = new Map();
+            mapQueryVaribles.set(queryVariable, [lineNumber]);
+            map.set(fileName, mapQueryVaribles);
         }
-
+        lastLineinserted = lineNumber;
+    
     
     }
     //console.log(map);
@@ -46,28 +82,26 @@ fs.readFile('output.txt', 'utf-8', (err, data) => {
     let run = 0;
     for (const [key, data] of map) {
         let arr = [];
-        
-        data.forEach((payload) => {
-           //console.log(payload);
-           // const gqldata = 'gql'+`${payload[0]}`;
-            arr.push(`{
-                request: {
-                    query: ${payload[1]},
-                    variables: ${payload[2]}
-                },
-                result: {
-                    data: {
+        const testAndLineArray=await scanTestAndLineNumberFromFile(`./${key}`);
+        //console.log(testAndLineArray);
+        // data.forEach((payload) => {
+        //    //console.log(payload);
+        //    // const gqldata = 'gql'+`${payload[0]}`;
+        //     arr.push(`{
+        //         request: {
+        //             query: ${payload[1]},
+        //             variables: ${payload[2]}
+        //         },
+        //         result: {
+        //             data: {
                         
-                    }
-                }
-            }`);
-        });
+        //             }
+        //         }
+        //     }`);
+        // });
            
-        // }
-        const outputArray = Array.from(new Set(data));
-       // console.log(outputArray);
-        const mocksVariable = `const MOCKS=[${arr}]`;
-        if(run==0)addMocksvariable(mocksVariable,key);
+        //const mocksVariable = `const MOCKS=[${arr}]`;
+        await addMocksvariable(data,key,testAndLineArray);
         run++;
         // fs.appendFile(`./${key}`, '\n'+content, (err) => {
         //     if (err) console.log(err);
--- reactVite/src/dog.test.tsx
+++ reactVite/src/dog.test.tsx
@@ -8,22 +8,7 @@ import { Dog } from "./dog";
 
 import { MockedProvider } from "@apollo/client/testing";
 
-// const setup = () => {
-//   <MockedProvider mocks={}></MockedProvider>
-// }
 
-// test("renders without error second", async () => {  
-//   //setup(MOCKS);
-//     render(
-
-//     //setup(mocks)
-//     <MockedProvider >
-//     <DogTest />
-//    </MockedProvider>
-//   );
-
-//  // expect(screen.getByText("Loading...")).toBeInTheDocument();
-// });
 
 test("renders without error second", async () => {
   render(<MockedProvider mocks={[]} addTypename={false}>
@@ -32,7 +17,7 @@ test("renders without error second", async () => {
   // expect(screen.getByText("Loading...")).toBeInTheDocument();
 });
 
-test("renders without error second", async () => {
+test("renders without error second2", async () => {
   render(<MockedProvider mocks={[]} addTypename={false}>
       <Dog name="Buck" name2="Buck2" name3="buck3" name4="Buck4" name5="Buck5" />
     </MockedProvider>);
